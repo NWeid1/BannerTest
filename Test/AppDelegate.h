438f79792d0186645d466311b2a42dff7d88b65a
Nick Weidinger <Nick@Nicks-MacBook-Pro.local>
2015-05-15 16:23:35 -0400

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Nick Weidinger on 5/15/15.
+//  Copyright (c) 2015 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Nick Weidinger on 5/15/15.
+//  Copyright (c) 2015 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
